f2a8b748f7560eb6ffb41c8ff000940a6a866d08
Artur <helter@wp.pl>
2023-01-23 17:39:49 +0100

    add article-container and card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
+import { ArticlesContainerComponent } from './components/articles-container/articles-container.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ArticlesContainerComponent, CardComponent],
   imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
