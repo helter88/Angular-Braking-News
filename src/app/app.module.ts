1f2976746a62dec674a822cc403b47a46581f8b3
Artur <helter@wp.pl>
2023-01-24 18:32:49 +0100

    add articles service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { SharedModule } from './modules/shared/shared.module';
 import { ContentContainerComponent } from './components/content-container/content-container.component';
 import { CardComponent } from './components/card/card.component';
 import { FilterArticlesContainerComponent } from './components/filter-articles-container/filter-articles-container.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, ContentContainerComponent, CardComponent, FilterArticlesContainerComponent],
-  imports: [BrowserModule, SharedModule],
+  declarations: [
+    AppComponent,
+    ContentContainerComponent,
+    CardComponent,
+    FilterArticlesContainerComponent,
+  ],
+  imports: [BrowserModule, SharedModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
