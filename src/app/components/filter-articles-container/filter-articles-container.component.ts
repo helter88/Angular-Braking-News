8207839cafa6cf8f63d79df0eaa16344a27224c5
Artur <helter@wp.pl>
2023-01-25 13:40:14 +0100

    handle selected country

--- src/app/components/filter-articles-container/filter-articles-container.component.ts
+++ src/app/components/filter-articles-container/filter-articles-container.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-filter-articles-container',
   templateUrl: './filter-articles-container.component.html',
-  styleUrls: ['./filter-articles-container.component.scss']
+  styleUrls: ['./filter-articles-container.component.scss'],
 })
 export class FilterArticlesContainerComponent {
-
+  onSelect(country: string) {
+    console.log(country);
+  }
 }
