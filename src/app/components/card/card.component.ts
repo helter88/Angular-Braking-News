b0f9fa77f5311e00128d7f7c0370ba33bd26709d
Artur <helter@wp.pl>
2023-01-27 20:58:37 +0100

    handle broken image src and switch for default in card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input } from '@angular/core';
 import { SelectedArticle } from 'src/app/models/article';
 
 @Component({
@@ -7,7 +7,16 @@ import { SelectedArticle } from 'src/app/models/article';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  noImagePath =
+    'https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg';
+
+  constructor(private el: ElementRef) {}
+
   @Input() article: SelectedArticle | undefined;
+
+  @HostListener('error') onError() {
+    this.el.nativeElement.src = this.noImagePath;
+  }
 
   handleClick(link: string) {
     window.open(link, '_blank', 'noopener,noreferrer');
