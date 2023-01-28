e373534d1acb581b5d69d80b9b22ae791cb72abe
Artur <helter@wp.pl>
2023-01-28 15:16:15 +0100

    change api url file

--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { combineLatest, map, Observable, switchMap } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 import {
   Article,
   Root,
